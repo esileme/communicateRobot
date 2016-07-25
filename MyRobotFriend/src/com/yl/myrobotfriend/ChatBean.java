e178079b1121016c9b17f44b0b2867add21a8120
esileme <280885685@qq.com>
2016-07-25 15:01:48 +0800

    这次，分析过程中出现的bug，一直提示说引用空指针，后来才发现view.inflate方法没有用，新的 bug，没有把说的话转换成文字打印到对话框中，下一个版本解决。

--- MyRobotFriend/src/com/yl/myrobotfriend/ChatBean.java
+++ MyRobotFriend/src/com/yl/myrobotfriend/ChatBean.java
@@ -1,14 +1,11 @@
 package com.yl.myrobotfriend;
 
-import android.R.integer;
-import android.R.string;
-
 public class ChatBean {
-	public string text;//文字的内容
+	public String text;//文字的内容
 	public boolean isAsker;//是否为提问者
 	public int imageId=-1;//定义图片id，若为-1则不现实图片
 	
-	public ChatBean(string text, boolean isAsker, int imageId) {
+	public ChatBean(String text, boolean isAsker, int imageId) {
 		this.text = text;
 		this.isAsker = isAsker;
 		this.imageId = imageId;
--- MyRobotFriend/src/com/yl/myrobotfriend/MainActivity.java
+++ MyRobotFriend/src/com/yl/myrobotfriend/MainActivity.java
@@ -2,17 +2,9 @@ package com.yl.myrobotfriend;
 
 import java.util.ArrayList;
 
-import com.iflytek.cloud.RecognizerResult;
-import com.iflytek.cloud.SpeechConstant;
-import com.iflytek.cloud.SpeechError;
-import com.iflytek.cloud.SpeechUtility;
-import com.iflytek.cloud.ui.RecognizerDialog;
-import com.iflytek.cloud.ui.RecognizerDialogListener;
-
+import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -20,6 +12,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+import com.yl.myrobotfriend.VoiceBean.WSBean;
 
 public class MainActivity extends Activity {
 	private ListView lvList;
@@ -69,7 +70,7 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		public Object getItem(int position) {
+		public ChatBean getItem(int position) {
 			return mChatList.get(position);
 		}
 
@@ -83,6 +84,9 @@ public class MainActivity extends Activity {
 			viewHolder holder;
 			if (coverView == null) {
 				holder = new viewHolder();// 初始化vierHolder
+				// 将界面显示出来，没有添加inflate代码导致崩溃。。。
+				coverView = View.inflate(MainActivity.this, R.layout.list_item,
+						null);
 				holder.tvAsk = (TextView) coverView.findViewById(R.id.tv_ask);
 				holder.llAnswer = (LinearLayout) coverView
 						.findViewById(R.id.ll_answer);
@@ -96,15 +100,16 @@ public class MainActivity extends Activity {
 				holder = (viewHolder) coverView.getTag();
 			}
 
-			ChatBean item = (ChatBean) getItem(position);// 强转
+			ChatBean item = getItem(position);// 强转
 
 			if (item.isAsker) {// 判断是不是提问者
-				holder.tvAnswer.setVisibility(View.VISIBLE);
+				holder.tvAsk.setVisibility(View.VISIBLE);
 				holder.llAnswer.setVisibility(View.GONE);
-
+				holder.tvAnswer.setText(item.text);
 			} else {
 				holder.tvAnswer.setVisibility(View.GONE);
 				holder.llAnswer.setVisibility(View.VISIBLE);
+				holder.tvAnswer.setText(item.text);
 				if (item.imageId != -1) {
 					holder.ivPic.setVisibility(View.VISIBLE);
 					holder.ivPic.setImageResource(item.imageId);
@@ -135,12 +140,24 @@ public class MainActivity extends Activity {
 	/**
 	 * set recognizerDialogListener listen
 	 */
+	StringBuffer mTextBuffer = new StringBuffer();
 	private RecognizerDialogListener recognizerDialogListener = new RecognizerDialogListener() {
 
 		@Override
 		public void onResult(RecognizerResult results, boolean isLast) {
 			System.out.println(results.getResultString());
 			System.out.println("isLast=" + isLast);
+
+			String text = parseData(results.getResultString());
+			mTextBuffer.append(text);
+
+			if (isLast) {// 会话结束
+				String finalText = mTextBuffer.toString();
+				mTextBuffer = new StringBuffer();// 清楚buffer
+				System.out.println("最终结果" + finalText);
+				mChatList.add(new ChatBean(finalText, true, -1));// 将回话内容添加进去
+				mAdapter.notifyDataSetChanged();// 刷新listview
+			}
 		}
 
 		@Override
@@ -148,4 +165,22 @@ public class MainActivity extends Activity {
 
 		}
 	};
+
+	/**
+	 * 解析语音字符串
+	 * 
+	 * @param resultString
+	 */
+
+	private String parseData(String resultString) {
+		Gson gson = new Gson();// 在不可用的情况下，在网上下载一个gson的jar包导入
+		VoiceBean bean = gson.fromJson(resultString, VoiceBean.class);
+		ArrayList<WSBean> ws = bean.ws;
+		StringBuffer sb = new StringBuffer();
+		for (WSBean wsBean : ws) {
+			String text = wsBean.cw.get(0).w;
+			sb.append(text);
+		}
+		return sb.toString();
+	}
 }
